f2ff7920df2951f4daa32d3458f1ec3fea1ab468
Manpreet <manpreetanything@gmail.com>
2019-01-21 17:43:42 +0530

    21-Jan-2019

--- app/src/main/java/com/suvidha/app/LeaveApplication.kt
+++ app/src/main/java/com/suvidha/app/LeaveApplication.kt
@@ -2,16 +2,21 @@ package com.suvidha.app
 
 import android.app.Activity
 import android.app.Dialog
+import android.graphics.Paint
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import com.adapter.app.LeavesAdapter
 import kotlinx.android.synthetic.main.leave_alert.*
 import kotlinx.android.synthetic.main.leave_application.*
+import android.graphics.Paint.UNDERLINE_TEXT_FLAG
+
+
 
 class LeaveApplication : Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.leave_application)
+        leaves_available.paintFlags = Paint.UNDERLINE_TEXT_FLAG
 
         leaves_available.setOnClickListener {
             openAlert()
